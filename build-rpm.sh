512c467b7ec79b5472419223192df3a1618a24bb
ChenthilVel <chenthilvel@users.noreply.github.com>
2018-04-20 15:21:54 +0530

    Update build-rpm.sh
    Allow different build versions for AppServerAgent & MachineAgent to be packaged into RPM.
    Capture build versions in RPM description

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VER=${VER:=4.2.14.0}
+VER=${VER:=4.4.3}
 ITER=${ITER:=1}
 UPL=${UPL:=0} # Disable Repo upload by default
 
-APP_ZIP="AppServerAgent-"${VER}".zip"
-MAC_ZIP="MachineAgent-"${VER}".zip"
+APP_ZIP="AppServerAgent-"${VER}"*.zip"
+MAC_ZIP="MachineAgent-"${VER}"*.zip"
 
 if ! ls /mnt/${APP_ZIP} &> /dev/null
 then
@@ -19,6 +19,8 @@ then
 	exit 1
 fi
 
+MAC_VERS=`cd /mnt && ls -1 $MAC_ZIP`
+APP_VERS=`cd /mnt && ls -1 $APP_ZIP`
 TMP_DIR="/tmp/appd/opt/appdynamics"
 mkdir -p ${TMP_DIR}
 mkdir -p ${TMP_DIR}/appserveragent
@@ -28,13 +30,19 @@ cp /mnt/${MAC_ZIP} ${TMP_DIR}
 cd ${TMP_DIR}/appserveragent
 unzip -q ../${APP_ZIP}
 rm -rf conf
+dname=`ls -1d ver${VER}*`
+if [[ "${dname}" != "ver${VER}" ]]
+then
+	echo "Creating symlink"
+	ln -s ${dname} ver${VER}
+fi
 cd ${TMP_DIR}/machineagent
 unzip -q ../${MAC_ZIP}
 rm ${TMP_DIR}/*.zip
 
 cd /root
 source /etc/profile.d/rvm.sh
-fpm -s dir -t rpm -C /tmp/appd --name appd --version ${VER} --iteration ${ITER} --before-install /root/preinstall --url "http://www.appdynamics.com/" --description "Repackaged AppDynamics AppServerAgent & MachineAgent into a single RPM" 
+fpm -s dir -t rpm -C /tmp/appd --name appd --version ${VER} --iteration ${ITER} --before-install /root/preinstall --url "http://www.appdynamics.com/" --description "Repackaged AppDynamics AppServerAgent: ${APP_VERS} & MachineAgent: ${MAC_VERS} into a single RPM" 
 
 RPM_FILE=appd-${VER}-${ITER}.x86_64.rpm 
 if [[ $? -eq 0 && -f ${RPM_FILE} ]]
